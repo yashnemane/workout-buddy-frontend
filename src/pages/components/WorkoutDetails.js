60baa99657474649032880d6a2fbb64746fc41cd
Yashkumar Nemane <96227576+yashnemane@users.noreply.github.com>
2024-04-04 15:45:07 +0530

    Update WorkoutDetails.js

--- src/pages/components/WorkoutDetails.js
+++ src/pages/components/WorkoutDetails.js
@@ -9,12 +9,7 @@ const WorkoutDetails = ({ workout }) => {
 
     const handleClick = async () => {
         const response = await fetch(`${backendBaseURL}api/workouts/` + workout._id, {
-            method: 'DELETE'.
-            headers: {
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
-              'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-            }
+            method: 'DELETE'
       })
 
         /*const response = await fetch('https://workout-buddy-backend-pmfl.onrender.com/api/workouts/' + workout._id, {
